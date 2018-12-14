4c6e0f2cb14452f26ad3a9444e47bf23121dfb26
Roberto Vaccaro <roberto.vaccaro@gmail.com>
2018-12-14 13:06:16 -0300

    Corregido nombre de componente

--- src/screens/Section.js
+++ src/screens/Section.js
@@ -3,10 +3,10 @@ import { View } from "react-native";
 
 import ArticlesList from '../components/ArticlesList';
 
-export default class HomeScreen extends Component {
+export default class SectionScreen extends Component {
   render() {
     const section = this.props.navigation.getParam('section');
-
+    
     return (
       <View style={{ flex: 1 }}>
         <ArticlesList type="section" section={section} />
